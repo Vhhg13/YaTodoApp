338d21173aa742a0327796789c0ad98e5ab91843
Vhhg13Win <maxvhhg@gmail.com>
2024-07-11 18:59:02 +0300

    Deletion implemented

--- app/src/main/java/tk/vhhg/todoyandex/datasource/local/db/TodoLocalDataSource.kt
+++ app/src/main/java/tk/vhhg/todoyandex/datasource/local/db/TodoLocalDataSource.kt
@@ -3,12 +3,15 @@ package tk.vhhg.todoyandex.datasource.local.db
 import android.util.Log
 import kotlinx.coroutines.flow.Flow
 import tk.vhhg.todoyandex.datasource.local.ITodoLocalDataSource
+import tk.vhhg.todoyandex.di.DeviceId
 import tk.vhhg.todoyandex.model.TodoItem
 import java.util.UUID
 import javax.inject.Inject
-import kotlin.math.sign
 
-class TodoLocalDataSource @Inject constructor(private val dao: TodoItemDao) : ITodoLocalDataSource {
+class TodoLocalDataSource @Inject constructor(
+    private val dao: TodoItemDao,
+    @DeviceId private val deviceId: String
+) : ITodoLocalDataSource {
     override val items: Flow<List<TodoItem>>
         get() = dao.getObservableTodoList()
 
@@ -43,20 +46,29 @@ class TodoLocalDataSource @Inject constructor(private val dao: TodoItemDao) : IT
     }
 
     override suspend fun merge(list: List<TodoItem>) {
-        val curMap = getCurrentList().associateBy { it.id }
-        val otherMap = list.associateBy { it.id }
-        val ids = curMap.keys + otherMap.keys
-        Log.d("trace list (ids)", ids.toString())
-        for(id in ids){
-            val curItem = curMap[id]
-            val otherItem = otherMap[id] ?: continue
-            if(curItem == null){
-                dao.add(listOf(otherItem))
-            }else if(otherItem.lastChange() > curItem.lastChange()){
-                dao.updateTodo(otherItem)
+        val localMap = getCurrentList().associateBy { it.id }
+        val remoteMap = list.associateBy { it.id }
+        val ids = localMap.keys + remoteMap.keys
+        for (id in ids) {
+            val localItem = localMap[id]
+            val remoteItem = remoteMap[id]
+            if (localItem != null && remoteItem != null) {
+                if (remoteItem.lastChange() > localItem.lastChange())
+                    dao.updateTodo(remoteItem)
+            } else if (localItem != null && remoteItem == null) {
+                if(localItem.lastUpdatedBy != deviceId){
+                    dao.remove(localItem)
+                }
+            } else if (localItem == null && remoteItem != null) {
+                if (remoteItem.lastUpdatedBy != deviceId) {
+                    dao.add(listOf(remoteItem))
+                }
+            } else {
+                throw IllegalStateException("Impossible situation")
             }
         }
         dao.getCurrentTodoList().also { Log.d("trace list (after merge)", it.toString()) }
     }
+
     private fun TodoItem.lastChange() = lastModificationDate ?: creationDate
 }
